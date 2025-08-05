73911b2e795e2061d98236467eb5a316a2bb7f96
alexcaceres655 <152577203+alexcaceres655@users.noreply.github.com>
2025-08-05 11:57:19 -0600

    Ruta añadida
    Se añadio post-form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PostFeed } from './components/post-feed/post-feed';
 import { TutoriasFeed } from './components/tutorias-feed/tutorias-feed';
 import { Login } from './components/login/login';
+import { PostForm } from './components/post-form/post-form';
 
 
 
@@ -9,6 +10,8 @@ export const routes: Routes = [
    { path: '', component: PostFeed},
     { path: 'post-feed', component: PostFeed},
   { path: 'tutorias-feed', component: TutoriasFeed},
-   { path: 'login', component: Login}
+   { path: 'login', component: Login},
+   { path: 'post-form', component: PostForm}
+
 ];
 
