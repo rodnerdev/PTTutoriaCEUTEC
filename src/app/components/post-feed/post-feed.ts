1f457205633598dc43fbabe7d9d961a21cbbec40
alexcaceres655 <152577203+alexcaceres655@users.noreply.github.com>
2025-08-09 17:32:59 -0600

    Navegacion de postfeed a postform

--- src/app/components/post-feed/post-feed.ts
+++ src/app/components/post-feed/post-feed.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { PostForm } from '../post-form/post-form';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-post-feed',
-  imports: [],
+  imports: [PostForm, RouterLink, RouterOutlet],
   templateUrl: './post-feed.html',
   styleUrl: './post-feed.scss'
 })
