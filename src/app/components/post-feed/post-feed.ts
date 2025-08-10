d8baebf6d3732708786320f9e5dcdcadd09eb535
alexcaceres655 <152577203+alexcaceres655@users.noreply.github.com>
2025-08-09 19:01:02 -0600

    Eliminar anuncios funcional en postfeed

--- src/app/components/post-feed/post-feed.ts
+++ src/app/components/post-feed/post-feed.ts
@@ -16,7 +16,7 @@ export class PostFeed {
 
 
 
-
+/*
 
 
 import { Component, inject } from '@angular/core';
@@ -49,3 +49,48 @@ export class PostFeed {
   }
 }
 
+*/
+
+
+
+import { Component, inject } from '@angular/core';
+import { PostForm } from '../post-form/post-form';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Anunciostgu } from '../../services/anunciostgu';
+
+@Component({
+  selector: 'app-post-feed',
+  imports: [PostForm, RouterLink, RouterOutlet, CommonModule],
+  templateUrl: './post-feed.html',
+  styleUrl: './post-feed.scss'
+})
+export class PostFeed {
+
+   anunciosService = inject(Anunciostgu);
+  anuncios: any[] = []; // Aquí se almacenarán los anuncios
+
+   constructor() {
+    this.cargarAnuncios();
+  }
+
+  cargarAnuncios() {
+    this.anunciosService.getAnuncios().subscribe((data) => {
+      this.anuncios = data;
+    });
+  }
+
+  // Función para eliminar con confirmación
+  eliminarAnuncio(uuid: string) {
+    if (confirm('¿Estás seguro de eliminar este anuncio?')) {
+      this.anunciosService.deleteAnuncio(uuid).then(() => {
+        this.cargarAnuncios(); // Recargar la lista después de eliminar
+      });
+    }
+  }
+
+  // Método para formatear la fecha (opcional)
+  formatDate(isoString: string) {
+    return new Date(isoString).toLocaleString();
+  }
+}
